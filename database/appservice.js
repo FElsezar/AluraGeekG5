1d5329cbdba00ca7c9586f3419ff20617bba2ef7
FElsezar <francoionelsezar@gmail.com>
2023-09-23 22:41:03 -0300

    build

---
+++ database/appservice.js
@@ -0,0 +1,54 @@
+const URL = 'https://alurageek-z2le.onrender.com';
+
+const listaProductos = () => fetch(`${URL}/producto`).then((respuesta) => respuesta.json());
+const listaCategorias = () => fetch(`${URL}/categoria`).then((respuesta) => respuesta.json());
+const usuario = () => fetch(`${URL}/usuario`).then((respuesta) => respuesta.json());
+
+//LEER/PARA LEER EL ID
+const producto = (id) => {
+    return fetch(`${URL}/producto/${id}`).then((respuesta) => respuesta.json());
+};
+
+//CREAR: POST
+const crearProducto = (nombre,precio,imagen,descripcion,categoria,idCategoria) => {
+    return fetch(`${URL}/producto`, { 
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({id: uuid.v4(),nombre,precio,imagen,descripcion,categoria,idCategoria}), 
+    })
+}
+
+//ACTUALIZAR: PUT
+const actualizarProducto = (nombre,precio,imagen,descripcion,categoria,idCategoria,id) => {
+    return fetch(`${URL}/producto/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({nombre,precio,imagen,descripcion,categoria,idCategoria})
+    })
+    .then(respuesta => respuesta)
+    .catch((error) => console.log(error));
+};
+
+//Borrar: DELETE
+const eliminarProducto = (id) => {
+    return fetch(`${URL}/producto/${id}`, {
+        method: 'DELETE'
+    })
+}
+
+
+
+
+export const clientServices = {
+    listaProductos,
+    usuario,
+    producto,
+    crearProducto,
+    actualizarProducto,
+    eliminarProducto,
+    listaCategorias,
+};
